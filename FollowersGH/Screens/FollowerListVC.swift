76b82c0f94c1eaecb9e4076f913aeccdcbab1af9
Jadiê Oliveira <jadieoliveira00@gmail.com>
2024-04-10 15:49:58 -0300

    Add Network Calls

--- FollowersGH/Screens/FollowerListVC.swift
+++ FollowersGH/Screens/FollowerListVC.swift
@@ -2,12 +2,23 @@ import UIKit
 
 class FollowerListVC: UIViewController {
 	
-	var userName: String?
+	var userName: String!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .systemPink
 		navigationController?.isNavigationBarHidden = false
 		navigationController?.navigationBar.prefersLargeTitles = true
+		
+		NetworkManager.shared.getFollower(for: userName, page: 1) { (followers, errorMessage) in
+			guard let followers else {
+				self.presentAlert(titleAlert: "Bad stuf happend", message: errorMessage!, titleButton: "Ok")
+				return
+			}
+			
+			print(followers.count)
+			dump(followers)
+			
+		}
 	}
 }
