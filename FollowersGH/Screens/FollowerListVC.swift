9fe5a42dc0bc1c6a5eb63031a8e1d9e0a2522a44
Jadiê Oliveira <jadieoliveira00@gmail.com>
2024-04-12 13:37:19 -0300

    Add uicollectionview

--- FollowersGH/Screens/FollowerListVC.swift
+++ FollowersGH/Screens/FollowerListVC.swift
@@ -3,13 +3,43 @@ import UIKit
 class FollowerListVC: UIViewController {
 	
 	var userName: String!
+	var collectionView: UICollectionView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		configureViewController()
+		configureCollectionView()
+		getFollowers()
+	
+	}
+	
+	private func configureViewController() {
 		view.backgroundColor = .systemPink
 		navigationController?.isNavigationBarHidden = false
 		navigationController?.navigationBar.prefersLargeTitles = true
+	}
+	
+	private func configureCollectionView() {
+		collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+		view.addSubview(collectionView)
+		collectionView.backgroundColor = .systemBlue
+		collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.identifier)
+	}
+	
+	private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+		let width = view.bounds.width
+		let padding: CGFloat = 12
+		let mininumItemSpacing:CGFloat = 10
+		let availableWidth = width - (padding * 2) - (mininumItemSpacing * 2)
+		let itemWidth = availableWidth / 3
 		
+		let flowLayout = UICollectionViewFlowLayout()
+		flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+		flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+		return flowLayout
+	}
+	
+	private func getFollowers() {
 		NetworkManager.shared.getFollower(for: userName, page: 1) { result in
 			switch result {
 			case .success(let followers):
@@ -21,4 +51,8 @@ class FollowerListVC: UIViewController {
 			
 		}
 	}
+	
+	
 }
+
+
